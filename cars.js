3852fb36ad87c4fda170680ca779aad60919e65a
dorrahsq <dorrah.saleh.elq.js@tuwaiq.edu.sa>
2021-10-19 11:35:52 +0300

    addclassway

--- cars.js
+++ cars.js
@@ -1,8 +1,4 @@
-// Create a file cars.js. Within cars.js:
-// o Create 5 car objects using a constructor function.
-// o Each car object should have the following properties: make, model, colour, image, registration number, price.
-// o Each car object should also include a editPrice(newPrice) method. this method should change the price of the car
-// o Each car object should also include a showAll() method. this method should display all the details about the specific car object.
+// task 1
 
 const Car = function (make, model, colour, image, egistrationnumber, price) {
   this.make = make;
@@ -26,4 +22,24 @@ const car3 = new Car("Toyota", "Avalon", "blue", "imge", 5594, 30000);
 const car4 = new Car("Toyota", "Corolla Hatchback", "red", "imge", 3594, 20000);
 const car5 = new Car("GMC", "Mercedes-Benz 600", "black", "imge", 2584, 60000);
 
-car5.showAll(); 
+class cars {
+  constructor(make, model, colour, image, egistrationnumber, price) {
+    this.make = make;
+    this.model = model;
+    this.colour = colour;
+    this.image = image;
+    this.egistrationnumber = egistrationnumber;
+    this.price = price;
+  }
+
+  editPrice(newPrice) {
+    this.price = newPrice;
+  }
+  showAll() {
+    console.log(
+      `car detiles is: ${this.make}  ${this.model}  ${this.colour}  ${this.image}   ${this.egistrationnumber} ${this.price}`
+    );
+  }
+}
+
+const car11 = new Car("mazda", "MAZDA2", "black", "imge", 3494, 100000);
